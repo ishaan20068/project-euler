72cfde8c6db0a19c457e71766590739f8fa825fa
Ishaan <ishaan20068@iiitd.ac.in>
2023-07-23 08:06:51 +0530

    largest palindrome product

--- 0004largestpalindromeproduct.cpp
+++ 0004largestpalindromeproduct.cpp
@@ -1,8 +1,6 @@
 // largest palindrome that can be formed by the product of two n digit numbers
 #include<bits/stdc++.h>
-#include <chrono>
 using namespace std;
-using namespace std::chrono;
 #define fast            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define int             long long
 #define o(x)            cout<<x<<endl;
