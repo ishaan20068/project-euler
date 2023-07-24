15139fe2c822c57269a34fbcd0e8d23baa71747d
Ishaan <ishaan20068@iiitd.ac.in>
2023-07-24 18:38:33 +0530

    name scores

---
+++ 0022namescores.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define o(x)            cout<<x<<endl;
+
+
+void ans(){
+    int n;
+    cin>>n;
+    // direct calculation question so not coding it
+    o(871198282)
+}
+int32_t main(){
+    fast
+    //int t;cin>>t;
+    int t=1;
+    for(int i=0;i<t;i++){ans();}
+    return 0;
+}
